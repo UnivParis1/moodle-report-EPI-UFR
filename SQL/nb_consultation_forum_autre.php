890acc17b4a063e1106a2def8346cf440d89076a
El-Miqui CHEMLALI <el-miqui.chemlali@univ-paris1.fr>
2017-06-14 10:48:25 +0200

    bug fix : stats nb consultation forum autre

--- SQL/nb_consultation_forum_autre.php
+++ SQL/nb_consultation_forum_autre.php
@@ -1,10 +1,11 @@
 <?php
  /* SOMME FORUMS_ANNONCES AUTRE */
  $SELECT= "	SELECT  distinct FR.id FROM {forum_read} FR
+                    INNER JOIN {forum} F ON F.id=FR.forumid
 		    INNER JOIN {forum_discussions} A ON FR.discussionid = A.id
 		    INNER JOIN {course} C ON A.course = C.id
 		    INNER JOIN {course_categories} CC ON C.category = CC.id
-		    WHERE A.type NOT LIKE 'news'
+		    WHERE F.type NOT LIKE 'news'
 		    AND (CC.path LIKE ? OR CC.path LIKE ? )";
 $count = true;
 $calculautre = false;
